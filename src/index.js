a983a97cc7a2dd39ea50f043c6af16fda509bc2e
Nicolas Jakob <nicow.jakob@gmail.com>
2017-04-22 12:15:10 +0200

    refactor: Use imports in package index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* @flow */
 
-export type {
-  Parcel,
-  Member,
-  Name,
-  Repository,
-  Bugs,
-} from './types';
-export { default as parseParcel } from './parseParcel';
+import type { Parcel, Member, Name, Repository, Bugs } from './types';
+import parseParcel from './parseParcel';
+
+export type { Parcel };
+export type { Member };
+export type { Name };
+export type { Repository };
+export type { Bugs };
+
+export { parseParcel };
