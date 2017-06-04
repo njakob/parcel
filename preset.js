b54c316c12d1301a00ddc4f553e4d8544563d95f
Nicolas Jakob <nicow.jakob@gmail.com>
2017-06-04 12:50:58 +0200

    build: Fix preset for different environments

--- preset.js
+++ preset.js
@@ -1,17 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-const babelPresetES2015 = require('babel-preset-es2015');
+const presetES2015 = require('babel-preset-es2015');
+const presetFlow = require('babel-preset-flow');
+const presetStage0 = require('babel-preset-stage-0');
+const pluginExternalHelpers = require('babel-plugin-external-helpers');
 
-module.exports = function buildPreset(context) {
-  const modules = (() => {
-    switch (process.env.MODULES) {
-      case 'cjs':
-        return 'commonjs';
-      case undefined:
-      default:
-        return false;
-    }
-  })();
+module.exports = function buildPreset() {
+  let modules = false;
+  let externalHelpers = false;
 
-  return babelPresetES2015.buildPreset(context, { modules });
+  if (process.env.PRESET === 'AVA') {
+    modules = 'commonjs';
+  }
+  if (process.env.PRESET === 'ROLLUP') {
+    externalHelpers = true;
+  }
+
+  return {
+    presets: [
+      presetStage0,
+      [presetES2015.buildPreset, { modules }],
+      presetFlow,
+    ],
+    plugins: [
+      externalHelpers && pluginExternalHelpers,
+    ].filter(Boolean),
+  };
 };
